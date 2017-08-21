0c6cdc2cbce9e35cb71035d21c4f4c7e58e83694
Dmitry Ananichev <a@qozz.ru>
2017-08-21 13:14:49 +0500

    Dockerfile update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,9 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
 	if [[ ! -s "$BITCOIN_DATA/bitcoin.conf" ]]; then
 		cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
 		printtoconsole=1
-		rpcallowip=::/0
+		listen=1
+		server=1
+		rpcallowip=0.0.0.0/0
 		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
 		rpcuser=${BITCOIN_RPC_USER:-bitcoin}
 		EOF
